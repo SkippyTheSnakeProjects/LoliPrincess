1a6e7b690b3decd38259a94f160270bf6ef080ea
Reid Mason <maddogshain132@gmail.com>
2019-07-19 20:00:06 +0100

    Added error printer and allowed command resubmit on edit

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from config import CMD_PREFIX, TOKEN
 import utils
 from utils import log
+from config import ERROR_DISPLAY_TIME
 
 bot = commands.AutoShardedBot(commands.when_mentioned_or(CMD_PREFIX))
 
@@ -10,6 +11,23 @@ bot = commands.AutoShardedBot(commands.when_mentioned_or(CMD_PREFIX))
 @bot.event
 async def on_ready():
     log(f"----- {bot.user.name} online -----")
+
+
+@bot.event
+async def on_message_edit(before, after):
+    # Resubmits and edited message as a command
+    await bot.on_message(after)
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    # Print the exception to the user
+    if isinstance(error, commands.CheckFailure):
+        try:
+            await ctx.message.delete()
+        except:
+            pass
+        await ctx.send(f"`{error}`", delete_after = ERROR_DISPLAY_TIME)
 
 
 log("----- Initializing -----")
