4ae6cd52622ae5fec1507a3f247790304854e36d
Reid Mason <maddogshain132@gmail.com>
2019-12-03 14:40:06 +0000

    Repaired leave message

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import discord
 from discord.ext import commands
 from config import CMD_PREFIX, TOKEN
 import utils
-from utils import log
 from config import ERROR_DISPLAY_TIME
 
 bot = commands.AutoShardedBot(commands.when_mentioned_or(CMD_PREFIX))
@@ -10,7 +9,7 @@ bot = commands.AutoShardedBot(commands.when_mentioned_or(CMD_PREFIX))
 
 @bot.event
 async def on_ready():
-    log(f"----- {bot.user.name} online -----")
+    utils.log(f"----- {bot.user.name} online -----")
 
 
 @bot.event
@@ -24,13 +23,12 @@ async def on_member_join(member):
 
 @bot.event
 async def on_member_remove(member):
-    # Load audit logs
-    logs = []
-    async for log in member.guild.audit_logs():
-        logs.append(log)
+    # Load audit log
+    log = await member.guild.audit_logs(limit = 1).flatten()
+    log = log[0]
 
     # Get the removers member object
-    target_id = logs[0].user.id
+    target_id = log.user.id
     for server_member in member.guild.members:
         if server_member.id == target_id:
             remover = server_member
@@ -40,13 +38,13 @@ async def on_member_remove(member):
     channel = member.guild.channels[0] if channel is None else channel
 
     # Triggers when user is kicked
-    if logs[0].action == discord.AuditLogAction.kick:
+    if log.action == discord.AuditLogAction.kick:
         await channel.send(
             content = f"Get fucked! {member.mention} has just been KICKED by {remover.mention}:wave::skin-tone-3:")
         utils.log(f"{member.display_name} was just kicked from {member.guild.name} by {remover.display_name}")
 
     # Triggers when user is banned
-    elif logs[0].action == discord.AuditLogAction.ban:
+    elif log.action == discord.AuditLogAction.ban:
         await channel.send(
             content = f"Get MEGA fucked! {member.mention} has just been BANNED by {remover.mention} :wave::skin-tone-3:")
         utils.log(f"{member.display_name} was just banned from {member.guild.name} by {remover.display_name}")
@@ -69,10 +67,10 @@ async def on_command_error(ctx, error):
     await ctx.send(f"`{error}`", delete_after = ERROR_DISPLAY_TIME)
 
 
-log("----- Initializing -----")
+utils.log("----- Initializing -----")
 # ----- Load all cogs from file
 loaded = utils.load_cogs(bot)
-log(f"Loaded {loaded} cog{'s' if loaded > 1 else ''}")
+utils.log(f"Loaded {loaded} cog{'s' if loaded > 1 else ''}")
 
 # Start bot 
 bot.run(TOKEN)
